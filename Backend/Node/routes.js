f67bbf3c0b9ee7be7d9766a20a88c485f649d694
= <deyvidholzt@gmail.com>
2021-09-14 10:32:32 -0300

    refactor: add token checking

--- Backend/Node/routes.js
+++ Backend/Node/routes.js
@@ -1,9 +1,22 @@
 import express from "express";
 import { getCategories } from "./api/get-categories.js";
-import { getCategoriesByPath } from "./api/get-categoriy-by-path.js";
+import { getCategoriesByPath } from "./api/get-category-by-path.js";
 import { searchCategories } from "./api/get-category.js";
+import { token } from "./token.js";
 
 const router = express.Router();
+
+router.use("/", (req, res, next) => {
+  if (!req.query.token) {
+    return res.status(401).json();
+  }
+
+  if (req.query.token !== token) {
+    return res.status(401).json();
+  }
+
+  next();
+});
 
 router.get("/", (req, res) => res.json({ message: "Hello World!" }));
 router.get("/categories/search-by-path/:browsePath", getCategoriesByPath);
