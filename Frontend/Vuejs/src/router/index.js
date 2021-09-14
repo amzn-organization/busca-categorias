4905c972ca2a30816ab6f4e309fe696cd70f0b87
Jorge Brunetto <jorge.brunetto@pixter.com.br>
2021-09-14 11:12:56 -0300

    chore: login

--- Frontend/Vuejs/src/router/index.js
+++ Frontend/Vuejs/src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
+    name: "Login",
+    component: Login,
+  },
+  
+  {
+    path: "/categorias",
     name: "Home",
     component: Home,
   },
