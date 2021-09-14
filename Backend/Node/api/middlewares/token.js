ca671864459737aa6ff358402675f3cf6e7cac14
= <deyvidholzt@gmail.com>
2021-09-14 11:12:33 -0300

    refactor: fix token middleware

---
+++ Backend/Node/api/middlewares/token.js
@@ -0,0 +1,11 @@
+export function isAuthenticated(req, res, next) {
+  if (!req.query.token) {
+    return res.status(401).json();
+  }
+
+  if (req.query.token !== token) {
+    return res.status(401).json();
+  }
+
+  next();
+}
--- Backend/Node/routes.js
+++ Backend/Node/routes.js
@@ -2,26 +2,19 @@ import express from "express";
 import { getCategories } from "./api/get-categories.js";
 import { getCategoriesByPath } from "./api/get-category-by-path.js";
 import { searchCategories } from "./api/get-category.js";
+import { isAuthenticated } from "./api/middlewares/token.js";
 import { token } from "./token.js";
 
 const router = express.Router();
 
-router.use("/categories", (req, res, next) => {
-  if (!req.query.token) {
-    return res.status(401).json();
-  }
-
-  if (req.query.token !== token) {
-    return res.status(401).json();
-  }
-
-  next();
-});
-
 router.get("/", (req, res) => res.json({ message: "Hello World!" }));
-router.get("/categories/search-by-path/:browsePath", getCategoriesByPath);
-router.get("/categories/search/:idOrName", searchCategories);
-router.get("/categories", getCategories);
+router.get(
+  "/categories/search-by-path/:browsePath",
+  isAuthenticated,
+  getCategoriesByPath
+);
+router.get("/categories/search/:idOrName", isAuthenticated, searchCategories);
+router.get("/categories", isAuthenticated, getCategories);
 
 router.get("/verify-token", (req, res) => {
   return res.json({ isValid: req.query.token === token });
